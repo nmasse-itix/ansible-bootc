4fb1ebfcf196064b85b742c0fad192c5fe860e5e
Nicolas Massé <nicolas.masse@itix.fr>
2026-06-17 13:53:05 +0200

    WiP

---
+++ examples/allinone/root/usr/local/bin/embed_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# See https://gitlab.com/fedora/bootc/examples/-/blob/main/physically-bound-images/README.md
+
+set -euxo pipefail
+
+image=$1
+additional_copy_args=${2:-""}
+
+mkdir -p /usr/lib/containers-image-cache
+sha=$(echo "$image" | sha256sum | awk '{ print $1 }')
+skopeo copy $additional_copy_args --preserve-digests docker://$image dir:/usr/lib/containers-image-cache/$sha
+echo "$image,$sha" >> /usr/lib/containers-image-cache/mapping.txt
